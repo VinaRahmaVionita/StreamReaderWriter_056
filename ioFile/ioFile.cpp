a0c4e72193a4e4843aed07afbb263e6cb13b3981
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-06-21 10:42:16 +0700

    menunjuk atau mmebuka  sebuah nama file yang bernama contohfile dan akan mencetak "pesan menulis file, q untuk keluar"

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -8,4 +8,9 @@ int main() {
 
 	//membuaka file dalam mode menulis 
 	ofstream outfile;
+	//menunjukkan ke sebuah nama file 
+	outfile.open("contohfile.txt");
+
+	cout << ">= menulis file, \'q\' untuk keluar" << endl;
+
 	
