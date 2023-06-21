ea971ce2991dded38b1583c210f82d951771ba51
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-06-21 10:58:59 +0700

    jika file tidak di temukan maka akan menampilkan pesan "unable to open file"

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -44,5 +44,7 @@ int main() {
 		//tutup file tersebut setelah selesai 
 		infile.close();
 	}
-	
+	//jika tidak di temukan file maka akan menampilkan ini
+	else cout << "unable to open file";
+	return 0;
 }
