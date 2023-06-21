7db70254c4158cc5594dda34a6088cebf6f8a3f5
VinaRahmaVionita <vinarahmavionita824@gmail.com>
2023-06-21 10:51:57 +0700

    menunjuk atau membuka sebuah file yang bernama contohfile.txt dan mencetak pesan membuka dan mencetak file

--- ioFile/ioFile.cpp
+++ ioFile/ioFile.cpp
@@ -28,4 +28,8 @@ int main() {
 
 	//membuka file dalam mode membaca
 	ifstream infile;
+	//menunjuk ke sebuah file 
+	infile.open("contohfile.txt");
+
+	cout << endl << ">= membuka dan membaca file" << endl;
 	
